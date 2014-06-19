db0d1520560c2b8ac2118194f86f7edd30aa0c02
Andy16823 <wagnerandreas17@yahoo.de>
2014-06-19 11:38:16 +0200

    Create Vector3.cs

---
+++ source/OpenObjectLoader/Types/Vector3.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenObjectLoader.Types
+{
+    public class Vector3
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public Vector3()
+        {
+            this.x = 0f;
+            this.y = 0f;
+            this.z = 0f;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
